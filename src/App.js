178e38cdce645bad43882b92f5d059a1d0fc4668
Tejendra Danu <Tejenderdanu46@gmail.com>
2024-06-12 13:25:13 +0530

    Initial Commits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { EmployeeData } from "./EmployeeData";
+
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    setData(EmployeeData);
+  }, []);
+
+  const handleEdit = (id) => {
+    alert(id);
+  }
+
+  const handleDelete = (id) => {
+    alert(id);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <td>Sr.No</td>
+            <td>Id</td>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Age</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{item.id}</td>
+                  <td>{item.firstName}</td>
+                  <td>{item.lastName}</td>
+                  <td>{item.age}</td>
+                  <td>
+                    <button className="btn btn-primary" onClick={() => handleEdit(item.id)}>Edit</button>
+                    &nbsp; | &nbsp;
+                    <button className="btn btn-danger" onClick={() => handleDelete(item.id)}>Delete</button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/EmployeeData.js
@@ -0,0 +1,27 @@
+export const EmployeeData = [
+    {
+        id: 1,
+        firstName: 'Tejen',
+        lastName: 'Danu',
+        age: 27
+    },
+    {
+        id: 2,
+        firstName: 'Rahul',
+        lastName: 'Sharma',
+        age: 27
+    },
+    {
+        id: 3,
+        firstName: 'Rishi',
+        lastName: 'Sunak',
+        age: 27
+    },
+    {
+        id: 4,
+        firstName: 'Rohit',
+        lastName: 'Patil',
+        age: 27
+    }
+]
+
